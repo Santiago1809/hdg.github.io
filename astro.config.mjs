4f02de67f0b58e0f62bcf62cd3c4cdcbca7b29c7
Santiago Aristizabal <aristizabalsantiago482@gmail.com>
2024-10-22 06:42:12 -0500

    Refactor Astro configuration and add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,13 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 
+import vercel from '@astrojs/vercel/serverless';
+
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   site: 'https://hdg.github.io',
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  adapter: vercel()
 });
